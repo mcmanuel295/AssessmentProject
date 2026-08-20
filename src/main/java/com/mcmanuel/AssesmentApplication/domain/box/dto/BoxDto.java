b849300067a5b1728d280636dc6364d9147e5284
unknown <mcmanuel755@gmail.com>
2026-08-20 01:02:51 +0100

    restructured project

--- src/main/java/com/mcmanuel/AssesmentApplication/domain/box/dto/BoxDto.java
+++ src/main/java/com/mcmanuel/AssesmentApplication/domain/box/dto/BoxDto.java
@@ -4,12 +4,10 @@ import com.mcmanuel.AssesmentApplication.domain.Camera;
 import com.mcmanuel.AssesmentApplication.domain.box.entity.Item;
 import com.mcmanuel.AssesmentApplication.domain.box.entity.State;
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.List;
 
 @Builder
-@Getter
 public record BoxDto(
         Integer boxId,
         String txRef,
--- src/main/java/com/mcmanuel/AssesmentApplication/domain/pojo/BoxRequest.java
+++ src/main/java/com/mcmanuel/AssesmentApplication/domain/pojo/BoxRequest.java
@@ -2,10 +2,8 @@ package com.mcmanuel.AssesmentApplication.domain.pojo;
 
 import com.mcmanuel.AssesmentApplication.domain.Camera;
 import com.mcmanuel.AssesmentApplication.domain.box.entity.Item;
-import lombok.Getter;
 
 import java.util.List;
-@Getter
 public record BoxRequest(
         String txRef,
         int weight,
